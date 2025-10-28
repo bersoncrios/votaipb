c05a1457ff2fe8d689b401a1ed3fe9b43a3205e8
bersons <bersonintech@gmail.com>
2025-10-28 15:18:10 -0300

    AJUSTE DE DEPLOY

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,28 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
-
-// Firebase moderno
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { provideDatabase, getDatabase } from '@angular/fire/database';
 import { environment } from './environments/environment';
 import { routes } from './app/app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt);
 
 bootstrapApplication(AppComponent, {
   providers: [
+    provideRouter(routes),
+
+    provideAnimations(),
+    { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
     provideRouter(routes),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
