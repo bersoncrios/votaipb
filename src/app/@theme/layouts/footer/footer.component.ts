55ba2d7bedebcfa0ebb22184364f1f72895eeffe
bersons <bersonintech@gmail.com>
2026-08-26 15:09:33 -0300

    feat: add beta version badge and legal disclaimer to authentication pages and footer

--- src/app/@theme/layouts/footer/footer.component.ts
+++ src/app/@theme/layouts/footer/footer.component.ts
@@ -1,8 +1,9 @@
-// angular import
 import { Component } from '@angular/core';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @Component({
   selector: 'app-footer',
+  imports: [SharedModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
